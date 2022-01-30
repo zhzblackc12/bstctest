e95004c32338076741804b3d42468e783abbb64e
zhzblackc <zhanghuazhong12@126.com>
2022-01-30 22:14:28 +0800

    btsc

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,7 +3,7 @@ import { AbstractConnector } from '@web3-react/abstract-connector'
 
 import { fortmatic, injected, portis, walletconnect, walletlink } from '../connectors'
 
-export const ROUTER_ADDRESS = '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D'
+export const ROUTER_ADDRESS = '0xA37Bd9Dd5d7ecDf5E7Ae2bbF8B003Cbc49BF70b0'
 
 // a list of tokens by chain
 type ChainTokenList = {
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -22,11 +22,12 @@ const ETHERSCAN_PREFIXES: { [chainId in ChainId]: string } = {
   3: 'ropsten.',
   4: 'rinkeby.',
   5: 'goerli.',
-  42: 'kovan.'
+  42: 'kovan.',
+  97: 'testnet.bscscan.com/'
 }
 
 export function getEtherscanLink(chainId: ChainId, data: string, type: 'transaction' | 'token' | 'address'): string {
-  const prefix = `https://${ETHERSCAN_PREFIXES[chainId] || ETHERSCAN_PREFIXES[1]}etherscan.io`
+  const prefix = `https://${ETHERSCAN_PREFIXES[chainId] || ETHERSCAN_PREFIXES[1]}`
 
   switch (type) {
     case 'transaction': {
